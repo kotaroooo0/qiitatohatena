9e714b987c7ea8048e95dbf4df96ef3b1f48b6d4
kotaroooo0 <k33asby@gmail.com>
2020-06-03 22:05:36 +0900

    Use ennsubet to inject .env

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,15 @@ FROM golang:latest
 RUN go get -u github.com/tenntenn/qiitaexporter
 RUN go get -u github.com/x-motemen/blogsync
 
+RUN apt-get update
+RUN apt-get install gettext-base -y
+
 WORKDIR /Documents
 ADD blogsync.template /Documents
 ADD setup.sh /Documents
 
 RUN mkdir -p ~/.config/blogsync
-ADD config.yaml /root/.config/blogsync
+ADD config.yaml.tmp /root/.config/blogsync
 
 RUN chmod +x setup.sh
 CMD ./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# TODO: 冗長
-sed -i -e "s/HATENA_BLOG_DOMAIN/${HATENA_BLOG_DOMAIN}/g" ~/.config/blogsync/config.yaml
-sed -i -e "s/HATENA_USER_NAME/${HATENA_USER_NAME}/g" ~/.config/blogsync/config.yaml
-sed -i -e "s/HATENA_API_KEY/${HATENA_API_KEY}/g" ~/.config/blogsync/config.yaml
+cat ~/.config/blogsync/config.yaml.tmp | envsubst > ~/.config/blogsync/config.yaml
 QIITA=${QIITA_API_KEY} qiitaexporter -template blogsync.template
 find posts -name "*.md" | xargs -I $ sh -c "blogsync post ${HATENA_BLOG_DOMAIN} < $"
