1e6f19c53d4d5b6cdf20cbe12e1ccfaf20d46152
kotaroooo0 <k33asby@gmail.com>
2020-06-04 00:36:57 +0900

    Pararell

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cat ~/.config/blogsync/config.yaml.tmp | envsubst > ~/.config/blogsync/config.yaml
 QIITA=${QIITA_API_KEY} qiitaexporter -template blogsync.template
-find posts -name "*.md" | xargs -I $ sh -c "blogsync post ${HATENA_BLOG_DOMAIN} < $"
+find posts -name "*.md" | xargs -I $ -P 5 sh -c "blogsync post ${HATENA_BLOG_DOMAIN} < $"
